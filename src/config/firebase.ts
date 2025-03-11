b31fcaf7002f2aac4c754aa94b0d2e17b5575071
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-03-11 17:55:39 -0600

    🔧 [CARPIL-23] firebase local config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,8 @@
 import { cert } from 'firebase-admin/app'
 import admin from 'firebase-admin'
+import firebaseJSONConfig from 'config/carpil-firebase-config.json'
 
-const firebaseConfig = process.env.FIREBASE_CONFIG ?? ''
+const firebaseConfig = process.env.FIREBASE_CONFIG ?? JSON.stringify(firebaseJSONConfig)
 
 if (admin.apps.length === 0) {
   admin.initializeApp({
