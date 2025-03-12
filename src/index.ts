90a7e25565d53a9962875896e031389cf30fb9ea
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-03-11 18:18:39 -0600

    🐛 [CARPIL-23] firebase dates to dates

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,23 @@ app.get('/rides/drivers/:id', authenticate, async (req, res) => {
     return
   }
 
-  const ride = rideRef.data() as Ride
+  const rideData = rideRef.data()
+  const ride: Ride = {
+    id: rideRef.id,
+    origin: rideData?.origin ?? null,
+    destination: rideData?.destination ?? null,
+    meetingPoint: rideData?.meetingPoint ?? null,
+    availableSeats: rideData?.availableSeats ?? 0,
+    price: rideData?.price ?? 0,
+    passengers: rideData?.passengers ?? [],
+    driver: rideData?.driver ?? null,
+    chatId: '',
+    departureDate: rideData?.departureDate?.toDate() ?? null,
+    deletedAt: rideData?.deletedAt?.toDate() ?? null,
+    createdAt: rideData?.createdAt?.toDate() ?? null,
+    updatedAt: rideData?.updatedAt?.toDate() ?? null
+  }
+
   res.json({ ride })
 })
 
